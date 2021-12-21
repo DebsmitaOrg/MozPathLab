3fc4d9448010999d25b5bdb0f8a9c7731493b9fb
Debsmita Paul <debosmitacoder@gmail.com>
2021-12-21 02:49:30 -0800

    added base files

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// javascript
+
+$('document').ready(function() {
+
+	$(".lazy").lazy();
+
+
+
+	
+  
+	
+
+});
+
+	
+
+
+$(window).on("load", function(){
+	$(".ajax_loader").addClass('hideloader');
+});
